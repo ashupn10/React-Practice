4137001440208dc59b64cf4ac95257dfe822b24a
ashupn10 <ashupn10@gmail.com>
2023-04-27 19:24:45 +0530

    Task 1

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,26 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>ExpanseItem</th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td>Food</td>
+          <td>Rs. 78</td>
+        </tr>
+        <tr>
+          <td>Petrol</td>
+          <td>Rs. 118</td>
+        </tr>
+        <tr>
+          <td>Movies</td>
+          <td>Rs. 778</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default ExpenseItem;
